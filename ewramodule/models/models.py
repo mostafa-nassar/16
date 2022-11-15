e8d7d1e8da60f6134c4e37c4cc87578abe99aae1
nooregy <ahmed887463@gmail.com>
2022-11-15 05:35:08 +0200

    xc

--- ewramodule/models/models.py
+++ ewramodule/models/models.py
@@ -66,8 +66,8 @@ class ResPartner(models.Model):
     # is_responsable = fields.Boolean(string='الجهة المسئولة')
     companytype=fields.Selection([('complainers','مقدم خدمة'),('responsable ','جهة المسئولة')],string="نوع الشركة")
     
-    # responsables=fields.Many2one(comodel_name="res.partner", string="الجهة المسئولة",)
-    # complainers=fields.One2many( string="مقدم خدمة",comodel_name="res.partner",inverse_name="responsables")
+    responsables=fields.Many2one(comodel_name="res.partner", string="الجهة المسئولة",)
+    complainers=fields.One2many( string="مقدم خدمة",comodel_name="res.partner",inverse_name="responsables")
    
     
    
