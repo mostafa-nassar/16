c48dcc6a3ee5e66e8d1cefee1dc17dafb3180e4f
nooregy <ahmed887463@gmail.com>
2022-11-15 02:31:24 +0200

    ewra module updates

--- ewramodule/models/helpdesk.py
+++ ewramodule/models/helpdesk.py
@@ -2,8 +2,20 @@ from odoo import models, fields,api ,_
 from odoo.tools import DEFAULT_SERVER_DATE_FORMAT as DF
 import base64
 from datetime import datetime, date, timedelta, time
+import secrets
 class ModelName(models.Model):
     _inherit = 'helpdesk.ticket'
+    #_sql_constraints = [('unique_code', 'unique(complaincode)', 'La référence doit être unique !')]
+    complaincode = fields.Char(string='رقم الشكوي', store=True,compute='_compute_complaincode' )
+    
+    @api.depends('name')
+    def _compute_complaincode(self):        
+        for record in self:
+            if record.id:
+                record.complaincode = str(secrets.token_hex(1))+str(record.id)
+    
+    
+   
     
     governate=fields.Many2one(comodel_name='ewra_governate',string='المحافظة')
     complaintype=fields.Many2one(comodel_name='ticket.classfication',string='نوع الشكوى')
@@ -35,24 +47,25 @@ class ModelName(models.Model):
         # user = self.env.user
         # self = self.sudo()
         values={
-            'complainername':vals['complainer'],
-            'national_id':vals['complainer_id'],
-            'governate':vals['governate'],
-            'city':vals['city'],
-            'adress':vals['adress'],
-            'complainer_tele':vals['tele'],
-            'complaineremail':vals['email'],
-            'responsableparty':vals['responsable'],
-            'serviceprovider':vals['introducer'],
-            'complaintype':vals['complain_class'],
-            'complainreason':vals['complain_type'],
-            'subscriptionnumber':vals['Subscription_id'],
-            'areanumber':vals['origin_num'],
-            'reportingway':vals['reportingway'],
-            'reportingcode':vals['reporting_id'],
-            'complaindate':vals['reportdate'],
-            'complainresponsedate':vals['replay_date'],
-            'providerresponse':vals['replay'],
+            'name': 'شكوي'+' '+ (vals['complainer'] if 'complainer' in vals else ''),
+            'complainername':vals['complainer'] if 'complainer' in vals else False,
+            'national_id':vals['complainer_id'] if 'complainer_id' in vals else False,
+            'governate':vals['governate'] if 'governate' in vals else False,
+            'city':vals['city'] if 'city' in vals else False,
+            'adress':vals['adress'] if 'adress' in vals else False,
+            'complainer_tele':vals['tele'] if 'tele' in vals else False,
+            'complaineremail':vals['email'] if 'email' in vals else False,
+            'responsableparty':vals['responsable'] if 'responsable' in vals else False,
+            'serviceprovider':vals['introducer'] if 'introducer' in vals else False,
+            'complaintype':vals['complain_class'] if 'complain_class' in vals else False,
+            'complainreason':vals['complain_type'] if 'complain_type' in vals else False,
+            'subscriptionnumber':vals['Subscription_id'] if 'Subscription_id' in vals else False,
+            'areanumber':vals['origin_num'] if 'origin_num' in vals else False,
+            'reportingway':vals['reportingway'] if 'reportingway' in vals else False,
+            'reportingcode':vals['reporting_id'] if 'reporting_id' in vals else False,
+            'complaindate':vals['reportdate'] if 'reportdate' in vals else False,
+            'complainresponsedate':vals['replay_date'] if 'replay_date' in vals else False,
+            'providerresponse':vals['replay'] if 'replay' in vals else False,
             # '':vals[''],
             # '':vals[''],
             
@@ -60,8 +73,37 @@ class ModelName(models.Model):
         # tmp_issue = self.env['helpdesk.ticket'].sudo().new(values)
         # values = tmp_issue._convert_to_write(tmp_issue._cache)
         myissue = self.env['helpdesk.ticket'].sudo().create(values)
+        #raise UserWarning(str(myissue.id))
         return {
-            'id': myissue.id
+            'code': """
+            
+            <div id="wrap">
+                    <div class="oe_structure oe_empty">
+                        <div class="container text-success oe_subdescription center center-content align-center text-center">
+                            <br/><br/>
+                            <div class="row justify-content-center">
+                           
+                                <div class="col-md-4">
+                                    <div class="alert alert-success">
+                                        تم ارسال الشكوي بنجاح
+                                        <i class="fa fa-thumbs-o-up fa-2x">
+                                        </i>
+
+                                        <p>
+                                            <h5>رقم الشكوي
+                                               """+myissue.complaincode+"""
+                                            </h5>
+                                        </p>
+                                    </div>
+                                  
+                                </div>
+                                
+                            </div>
+                        </div>
+                    </div>
+                </div>
+              
+            """
         }
     
 
--- ewramodule/static/src/js/ewra_ticketsubmit.js
+++ ewramodule/static/src/js/ewra_ticketsubmit.js
@@ -44,7 +44,7 @@ publicWidget.registry.complainInit= publicWidget.Widget.extend({
                 $("#complain_class").empty();
                var classes=result['classfications'];
                if(classes){
-                $("#complain_class").append("<option>أختر واحد </option>");
+                $("#complain_class").append("<option value=''>أختر واحد </option>");
                 for(let i=0;i<classes.length;i++){
                     $("#complain_class").append("<option value='"+classes[i].id+"'>"+classes[i].name+"</option>");
                 }
@@ -53,7 +53,7 @@ publicWidget.registry.complainInit= publicWidget.Widget.extend({
             $("#governate").empty();
             var governments=result['governments'];
             if(governments){
-             $("#governate").append("<option>أختر واحد </option>");
+             $("#governate").append("<option value=''>أختر واحد </option>");
              for(let i=0;i<governments.length;i++){
                  $("#governate").append("<option value='"+governments[i].id+"'>"+governments[i].name+"</option>");
              }
@@ -62,7 +62,7 @@ publicWidget.registry.complainInit= publicWidget.Widget.extend({
             $("#responsable").empty();
             var responsapleparty=result['responsapleparty'];
             if(responsapleparty){
-             $("#responsable").append("<option>أختر واحد </option>");
+             $("#responsable").append("<option value=''>أختر واحد </option>");
              for(let i=0;i<responsapleparty.length;i++){
                  $("#responsable").append("<option value='"+responsapleparty[i].id+"'>"+responsapleparty[i].name+"</option>");
              }
@@ -72,7 +72,7 @@ publicWidget.registry.complainInit= publicWidget.Widget.extend({
             
             var reportingway=result['reportingway'];
             if(reportingway){
-             $("#reportingway").append("<option>أختر واحد </option>");
+             $("#reportingway").append("<option value=''>أختر واحد </option>");
              for(let i=0;i<reportingway.length;i++){
                  $("#reportingway").append("<option value='"+reportingway[i].id+"'>"+reportingway[i].name+"</option>");
              }
@@ -90,7 +90,7 @@ publicWidget.registry.complainInit= publicWidget.Widget.extend({
         events: { 
             'change .complain_class':'changecomplainclass',
             'change #responsable':'responsablechange',
-            'click #sendhelpdesk':'SubmitHelpDeskForm',
+            'submit #complainform':'SubmitHelpDeskForm',
             
         },
         changecomplainclass:function(event){
@@ -101,7 +101,7 @@ publicWidget.registry.complainInit= publicWidget.Widget.extend({
                 params: {'id':$('.complain_class').val()},
             }).then(function (result) {
                var types=result['types'];
-                $("#complain_type").append("<option>أختر واحد </option>");
+                $("#complain_type").append("<option value=''>أختر واحد </option>");
                 for(let i=0;i<types.length;i++){
                     $("#complain_type").append("<option value='"+types[i].id+"'>"+types[i].name+"</option>");
                 }
@@ -117,7 +117,7 @@ publicWidget.registry.complainInit= publicWidget.Widget.extend({
                 params: {'id':$('#responsable').val()},
             }).then(function (result) {
                var types=result['types'];
-                $("#introducer").append("<option>أختر واحد </option>");
+                $("#introducer").append("<option value=''>أختر واحد </option>");
                 for(let i=0;i<types.length;i++){
                     $("#introducer").append("<option value='"+types[i].id+"'>"+types[i].name+"</option>");
                 }
@@ -126,9 +126,46 @@ publicWidget.registry.complainInit= publicWidget.Widget.extend({
         },
 
 
-        SubmitHelpDeskForm:function(event){
-    //alert("ddd");
-    //return;
+        SubmitHelpDeskForm:function(e){
+            e.preventDefault();
+            e.stopPropagation();
+         var self=this;
+         this._rpc({
+            model: 'helpdesk.ticket',
+            method: 'create_help_ticket_portal',
+            args: [{
+                'complainername':$("#complainername").val(),
+                'national_id':$("#national_id").val(),
+                'governate':$("#governate").find(":selected").val(),
+                'city':$("#city").val(),
+                'adress':$("#adress").val(),
+                'complainer_tele':$("#complainer_tele").val(),
+                'complaineremail':$("#complaineremail").val(),
+                'responsableparty':$("#responsableparty").find(":selected").val(),
+                'serviceprovider':$("#serviceprovider").find(":selected").val(),
+                'complaintype':$("#complaintype").find(":selected").val(),
+                'complainreason':$("#complainreason").find(":selected").val(),
+                'subscriptionnumber':$("#subscriptionnumber").val(),
+                'areanumber':$("#areanumber").val(),
+                'reportingway':$("#reportingway").find(":selected").val(),
+                'reportingcode':$("#reportingcode").val(),
+                'complaindate':$("#complaindate").val(),
+                'complainresponsedate':$("#complainresponsedate").val(),
+                'complainername':$("#providerresponse").val(),
+
+
+            }],
+        }).then(function (response) {
+            if (response.errors) {
+                toastr.error('Something went wrong ' + response.errors + ' , try again.')
+                return Promise.reject(response);
+            } else {
+                
+                $(".complainform").html(response['code']).fadeIn(500);
+            }
+        });
+    
+    return  false;
         }
     
 
